34112789213100091063339dfc73402258d52aab
Mark H. Jones <markjones112358@gmail.com>
2013-12-08 09:11:47 +1300

    Added automatic installer

---
+++ setup.sh
@@ -0,0 +1,50 @@
+user=$SUDO_USER
+path=`pwd`
+if [ ! -w /usr/bin ]
+then
+    echo "This script requires elevated priveledges - run with sudo"
+    exit
+fi
+echo "Checking for dmenu installation..."
+if [ ! -e /usr/bin/dmenu ]
+then
+    echo "dmenu not already installed - Installing"
+    echo ""
+    if [ -e /usr/bin/apt-get ]
+    then
+        sudo apt-get install build-essential libx11-dev libxinerama-dev
+        cd /tmp
+        wget http://dl.suckless.org/tools/dmenu-4.5.tar.gz
+        tar -xzf dmenu-4.5.tar.gz
+        cd dmenu-4.5
+        make
+        sudo make install
+        echo "dmenu has been installed from source"
+    elif [ -e /usr/bin/yum ]
+    then
+        sudo yum install dmenu
+        echo "dmenu has been installed from yum"
+    elif [ -e /usr/bin/pacman ]
+    then
+        sudo pacman -S dmenu
+        echo "dmenu has been installed from pacman"
+    else
+        echo "Could not determine package manager and dmenu not installed - Aborting"
+        exit
+    fi
+else
+    echo "dmenu has been found - will not install"
+    echo "NOTE: dmenu 4.5 is recommended for the best searching capability"
+fi
+echo ""
+echo "Installing dmenu-extended..."
+cd "$path"
+sudo python setup.py install
+echo "Done!"
+echo ""
+dmenu_extended_build
+echo ""
+echo "Creating signature file..."
+curl https://github.com/markjones112358/dmenu-extended/archive/master.zip | sha1sum | awk '{print $1}' > /home/$SUDO_USER/.config/dmenu-extended/signature.txt
+echo "Done!"
+echo ""
