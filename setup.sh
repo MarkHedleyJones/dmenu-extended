b58b80e4c7302462c09157cdf1bf3c0ba0a42b9f
Mark H. Jones <markjones112358@gmail.com>
2014-09-10 11:52:28 +1200

    Quoted variables and paths and added comments

--- setup.sh
+++ setup.sh
@@ -1,23 +1,28 @@
-user=$SUDO_USER
-path=`pwd`
+INSTALL_PATH=`pwd`
 
-if [ ! -e /usr/bin/python ]
+# Check that Python is installed and exit if not
+if [ ! -e "/usr/bin/python" ]
 then
     echo "You must have Python installed"
     exit
 fi
 
-if [ ! -w /usr/bin ]
+# Check that we have permission to write to /usr/bin
+# for moving the scripts and running the distutils setup
+if [ ! -w "/usr/bin" ]
 then
     echo "This script requires elevated priveledges - run with sudo"
     exit
 fi
+
+# Check for an installed version of dmenu
+# Attempt to install dmenu if it is not found
 echo "Checking for dmenu installation..."
-if [ ! -e /usr/bin/dmenu ]
+if [ ! -e "/usr/bin/dmenu" ]
 then
     echo "dmenu not already installed - Installing"
     echo ""
-    if [ -e /usr/bin/apt-get ]
+    if [ -e "/usr/bin/apt-get" ]
     then
         sudo apt-get install build-essential libx11-dev libxinerama-dev
         cd /tmp
@@ -44,24 +49,37 @@ else
     echo "NOTE: dmenu 4.5 is recommended for the best searching capability"
 fi
 echo ""
+
+# Run the distutils python setup
 echo "Installing dmenu-extended..."
-cd "$path"
+cd "$INSTALL_PATH"
 sudo python setup.py install
 echo "Done!"
 echo ""
-su $SUDO_USER -c "python -c 'import dmenu_extended
+
+# Run the dmenu_extended library to trigger configuration folder creation
+su "$SUDO_USER" -c "python -c 'import dmenu_extended
 '"
 echo ""
-echo "Creating signature file..."
-if [ -O /home/$SUDO_USER/.config/dmenu-extended/signature.txt ]
-then
-    sudo chown $SUDO_USER /home/$SUDO_USER/.config/dmenu-extended/signature.txt
-fi
+
+# 
+#echo "Creating signature file..."
+#if [ -O "/home/$SUDO_USER/.config/dmenu-extended/signature.txt" ]
+#then
+#    sudo chown "$SUDO_USER" "/home/$SUDO_USER/.config/dmenu-extended/signature.txt"
+#fi
+
+# Create the signature file for the currently installed version.
+# This is used to detect if there are updates. This is a messy way
+# of doing this as it requires downloading the whole archive again.
 echo "Downloading zip file to create sha1sum signature..."
-su $user -c "curl https://github.com/markjones112358/dmenu-extended/archive/master.zip | sha1sum | awk '{print $1}' > /home/$SUDO_USER/.config/dmenu-extended/signature.txt"
+su "$SUDO_USER" -c "curl https://github.com/markjones112358/dmenu-extended/archive/master.zip | sha1sum | awk '{print $1}' > /home/$SUDO_USER/.config/dmenu-extended/signature.txt"
 echo "Done!"
 echo ""
-if [ -e /home/$SUDO_USER/.config/dmenu-extended/cache/dmenuExtended_main.txt ]
+
+# Check to see if a cache file already exists and if not
+# prompt the user to build the cache
+if [ -e "/home/$SUDO_USER/.config/dmenu-extended/cache/dmenuExtended_main.txt" ]
 then
     echo "Existing cache file found, skipping cache rebuild"
 else
@@ -83,14 +101,18 @@ else
     while true; do
         read -p "Would you like to build the cache now? [y/n]: " yn
         case $yn in
-            [Yy]* ) su $user -c "dmenu_extended_build"; break;;
+            [Yy]* ) su "$SUDO_USER" -c "dmenu_extended_build"; break;;
             [Nn]* ) echo "Build the cache at any time by executing 'dmenu_extended_build'"; break;;
             * ) echo "Please select either 'yes' or 'no'.";;
         esac
     done
 fi
 echo ""
-if [ ! -e /home/$SUDO_USER/.config/dmenu-extended/plugins/dmenuExtended_settings.py ]
+
+# Check to see if the dmenuExtended_settings plugin is installed and if not
+# ask the user if he/she would like to install it. If yes, download and
+# install it.
+if [ ! -e "/home/$SUDO_USER/.config/dmenu-extended/plugins/dmenuExtended_settings.py" ]
 then
     echo "*******************************************************************"
     echo "*     You do not have the extended settings plugin installed      *"
@@ -106,9 +128,9 @@ then
     while true; do
         read -p "Would you like to install this plugin now? [y/n]: " yn
         case $yn in
-            [Yy]* ) cd /home/$SUDO_USER/.config/dmenu-extended/plugins;
-                su $SUDO_USER -c "wget https://gist.github.com/markjones112358/7700097/raw/dmenuExtended_settings.py"
-                su $SUDO_USER -c "python -c 'import dmenu_extended
+            [Yy]* ) cd "/home/$SUDO_USER/.config/dmenu-extended/plugins";
+                su "$SUDO_USER" -c "wget https://gist.github.com/markjones112358/7700097/raw/dmenuExtended_settings.py"
+                su "$SUDO_USER" -c "python -c 'import dmenu_extended
 a=dmenu_extended.dmenu()
 a.plugins_available()'"
                 break;;
