d4c5b44c351c0aadb7319324d30f33b74d3ab830
Stephen <stephen@pynenburg.ca>
2018-01-20 01:24:16 -0500

    systemd installer should work even if dmenu-extended installer isn't used

--- systemd-install.sh
+++ systemd-install.sh
@@ -15,12 +15,20 @@ do
 	esac
 done
 
+DECB_NONDEFAULT=false;
+
 if [ "$PER_USER_INSTALL" == "YES" ] ; then
 	SCRIPT_PATH=$HOME/.local/share/systemd/user
-	SERVICE_UNIT_NAME=update-dmenu-extended-db.user.service
+	DECB_DEFAULT=~/.local/bin/dmenu_extended_cache_build
+	if [ ! -f $DECB_DEFAULT ]; then		
+		DECB_NONDEFAULT=true
+	fi
 else
 	SCRIPT_PATH=/usr/lib/systemd/user
-	SERVICE_UNIT_NAME=update-dmenu-extended-db.system.service
+	DECB_DEFAULT=/usr/bin/dmenu_extended_cache_build
+	if [ ! -f DECB_DEFAULT ]; then
+		DECB_NONDEFAULT=true
+	fi
 fi
 
 if [ "$UNINSTALL" == "YES" ]; then
@@ -35,5 +43,35 @@ if [ ! -d "$SCRIPT_PATH" ]; then
 fi
 
 echo "Installing systemd service in $SCRIPT_PATH..."
-cp -v systemd/$SERVICE_UNIT_NAME $SCRIPT_PATH/update-dmenu-extended-db.service
+
+if [ $DECB_NONDEFAULT ]; then
+	echo "dmenu_extended_cache_build not found at the default location."
+	echo "Please enter the full path to dmenu_extended_cache_build:"
+	read DECB_PATH
+else
+	DECB_PATH = $DECB_DEFAULT
+fi;
+
+# If per-user install, we need systemd to call dmenu_extended_cache build with /bin/sh -c
+if [ "$PER_USER_INSTALL" == "YES" ]; then
+	SYSTEMD_EXEC='/bin/sh -c "$DECB_PATH"'
+else
+	SYSTEMD_EXEC=$DECB_PATH
+fi
+
+# Clear old file (if exists) then (re)write
+if [ -f $SCRIPT_PATH/update-dmenu-extended-db.service ]; then
+	rm $SCRIPT_PATH/update-dmenu-extended-db.service
+fi
+
+cat <<EOT >> $SCRIPT_PATH/update-dmenu-extended-db.service
+[Unit]
+Description=Updates the database file used by dmenu_extended_run
+
+[Service]
+Type=oneshot
+User=%i
+ExecStart=$SYSTEMD_EXEC
+EOT
+echo "$SCRIPT_PATH/update-dmenu-extended-db.service created."
 cp -v systemd/update-dmenu-extended-db.timer $SCRIPT_PATH
