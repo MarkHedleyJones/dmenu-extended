9a6df567767bf620fa857bc58f414d821a27d4af
Mark Hedley Jones <markhedleyjones@gmail.com>
2019-08-24 12:14:03 +0900

    Removed unused versioning logic

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 import os
 from distutils.core import setup
 
-# __version__ = None
-# initFile = os.path.join(os.path.dirname(__file__), 'dmenu_extended', '__init__.py')
-# for line in open(initFile).readlines():
-#     if (line.startswith('version_info') or line.startswith('__version__')):
-#         exec(line.strip())
-
-# print("This is dmenu_extended v"+__version__)
-
 setup(name='dmenu_extended',
       version='0.1.0',
       description='A wrapper for dmenu that implements plugins and fast file/folder searching',
