96487cc40d12e8a3047898d2f719da64ba821dea
Mark Hedley Jones <markhedleyjones@gmail.com>
2019-08-25 11:33:29 +0900

    Added test to reproduce issue #88

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf8 -*-
 
+import mock
 import pytest
 import sys
 from os import path
@@ -10,7 +11,7 @@ import dmenu_extended as d
 
 menu = d.dmenu()
 
-def command_to_list():
+def test_command_to_list():
     assert menu.command_to_list(['a', 'b', 'c']) == [u'a', u'b', u'c']
     assert menu.command_to_list('a b c') == [u'a', u'b', u'c']
     assert menu.command_to_list(['a', 'b c']) == [u'a', u'b', u'c']
@@ -29,3 +30,8 @@ def command_to_list():
     assert menu.command_to_list('xdg-open /home/user/aö/foldername/') == [u'xdg-open', u'/home/user/a\xf6/foldername/']
     assert menu.command_to_list('xdg-open "/home/user/aö/"foldernam "e/"') == [u'xdg-open', u'/home/user/a\xf6/foldernam', u'e/']
     assert menu.command_to_list('xdg-open "/home/user/1983 - BVerfG - Volkszahlungsurteil - 1983.pdf"') == [u'xdg-open', u'/home/user/1983 - BVerfG - Volkszahlungsurteil - 1983.pdf']
+
+
+def test_scan_binaries_file_in_system_path():
+    with mock.patch.object(menu, 'system_path', new=lambda: [u'/bin', u'/bin/cp'] ):
+        assert type(menu.scan_binaries()) == list
