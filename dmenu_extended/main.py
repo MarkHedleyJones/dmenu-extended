599d4669b56c8a622e62d5d2ae2980b12f729376
Mark Jones <markhedleyjones@gmail.com>
2019-08-28 08:27:59 +0900

    Modified printing of plugin import exception, removed delete

--- dmenu_extended/main.py
+++ dmenu_extended/main.py
@@ -248,12 +248,7 @@ def load_plugins(debug=False):
                     plugins_loaded[-1]['plugin'].debug = True
                     print('Loaded plugin ' + plugin)
             except Exception as e:
-                if debug:
-                    print('Error loading plugin ' + plugin)
-                    print(str(e))
-                else:
-                    print('!! Plugin was deleted to prevent interruption to dmenuExtended')
-                    os.remove(path_plugins + '/' + plugin + '.py')
+                print('Error loading plugin ' + plugin + ': ' + str(e))
 
     return plugins_loaded
 
