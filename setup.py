8459f5ae9b13c4a226a3856b53a57c0872aed278
Mark Hedley Jones <MarkHedleyJones@gmail.com>
2022-05-20 22:39:56 +0900

    Update setup.py to use setuptools

--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
 #! /usr/bin/env python
 # -*- coding: utf8 -*-
-import os
-from distutils.core import setup
+import setuptools
 
-setup(name='dmenu_extended',
-      version='0.1.0',
-      description='A wrapper for dmenu that implements plugins and fast file/folder searching',
-      author='Mark H. Jones',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='dmenu_extended',
+      version='0.2.0',
+      author='Mark Hedley Jones',
       author_email='markhedleyjones@gmail.com',
+      description='A wrapper for dmenu that implements plugins and fast file/folder searching',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/markhedleyjones/dmenu-extended',
-      packages=['dmenu_extended'],
-      scripts=['scripts/dmenu_extended_run', 'scripts/dmenu_extended_cache_build'],
+      packages=setuptools.find_packages(),
+      classifiers=[
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6',
+      entry_points={
+            'console_scripts': [
+                  'dmenu_extended_run=dmenu_extended.main:run',
+                  'dmenu_extended_build_cache=dmenu_extended.main:build_cache',
+            ]
+      },
       data_files=[('share/dmenu-extended/', ['scripts/systemd-install.sh']),
                   ('share/dmenu-extended/systemd', ['systemd/update-dmenu-extended-db.timer'])]
-      )
+)
