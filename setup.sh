fc0dc6603e0dd2ad00adb08c33fde1973017281b
Mark Hedley Jones <MarkHedleyJones@gmail.com>
2022-07-27 21:55:09 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 
 set -eu
 
+cleanup() {
+    rm -rf ./dist ./src/dmenu_extended.egg-info
+}
+
+cleanup
 python3 -m build --wheel
 pip3 install dist/dmenu_extended-*-py3-none-any.whl --upgrade
-rm -rf dist src/dmenu_extended.egg-info
+cleanup
