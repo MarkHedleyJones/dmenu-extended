f9c3927324059ef8d37a9182eb0bfd6795c7055e
Mark Hedley Jones <MarkHedleyJones@gmail.com>
2022-08-13 13:38:44 +0100

    Add a check that the version string has been incremented correctly

--- test.sh
+++ test.sh
@@ -4,6 +4,11 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 set -eu
 
+build_image() {
+  docker build -f ${script_dir}/tests/Dockerfile -t dmenu-extended-test:latest .
+  return $?
+}
+
 usage() {
   cat <<EOF
 Usage: $(
@@ -14,9 +19,10 @@ Run dmenu-extended tests
 
 Available options:
 
--h, --help      Print this help and exit
--f, --full      Run full test-suite (requires docker)
--b, --build     Build the docker image to run system tests (requires docker)
+-h, --help           Print this help and exit
+-f, --full           Run full test-suite (requires docker)
+-b, --build          Build the docker image to run system tests (requires docker)
+-c, --check-version  Check that the version defined in pyproject.toml is higher than that listed on pypi
 EOF
   exit
 }
@@ -24,11 +30,13 @@ EOF
 parse_params() {
   full=0
   build=0
+  check_version=0
   while :; do
     case "${1-}" in
     -h | --help) usage ;;
     -f | --full) full=1 ;;
     -b | --build) build=1 ;;
+    -c | --check-version) check_version=1 ;;
     -?*)
       echo "Unknown option: $1"
       exit 1
@@ -41,13 +49,19 @@ parse_params() {
   return 0
 }
 
-build_image() {
-  docker build -f ${script_dir}/tests/Dockerfile -t dmenu-extended-test:latest .
-  return $?
-}
-
 parse_params "$@"
 
+if [ "${check_version}" -eq 1 ]; then
+  version=$(grep -E '^version =' ${script_dir}/pyproject.toml | cut -d '"' -f 2)
+  pypi_version=$(curl -s https://pypi.org/pypi/dmenu-extended/json | jq -r '.info.version')
+  if ${script_dir}/tests/check_version_string.sh "${version}" "${pypi_version}"; then
+    echo "The version in pyproject.toml (${version}) has been correctly incremented relative to the version on pypi (${pypi_version})"
+    exit 0
+  else
+    echo "The version in pyproject.toml (${version}) has not been incremented relative to the current version on pypi (${pypi_version})"
+    exit 1
+  fi
+fi
 if [ "${build}" -eq 1 ] || [ "${full}" -eq 1 ]; then
   if [ "${build}" -eq 1 ]; then
     if ! build_image; then
